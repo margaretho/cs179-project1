013b0c0349c2171c02723591cc63ea0bc1802f09
margaretho <margaretho@users.noreply.github.com>
2015-02-11 19:07:37 -0500

    make weather sticky button post new note

--- scripts/03.js
+++ scripts/03.js
@@ -1,20 +1,5 @@
 src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"
 
-    var foo = document.getElementById("error");
-    
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getWeather);
-        } else {
-            foo.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
-
-    function getWeather(position) {
-        var url = 'api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
-        foo.innerHTML = url;
-    }
-
 $(document).ready(function(){
 
     $("button#new").click(function(){
@@ -22,6 +7,29 @@ $(document).ready(function(){
         $( ".container" ).append('<div class="sticky"> <button id="close">x</button><p>' + text + '</p></div>');
         $("button#close").click(function(){
         this.parentNode.parentNode.removeChild(this.parentNode);
+    });
+    });
+
+    $("button#weather").click(function(){
+        var text = $('#new-sticky').val();
+        $( ".container" ).append('<div class="sticky"> <button id="close">x</button><p>' + text + '</p></div>');
+        $("button#close").click(function(){
+        this.parentNode.parentNode.removeChild(this.parentNode);
+
+        var message = document.getElementById("error");
+    
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(getWeather);
+            } else {
+                message.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function getWeather(position) {
+            var url = 'api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
+            message.innerHTML = url;
+        }
     });
     });
     
