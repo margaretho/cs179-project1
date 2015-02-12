f16d1f7c9582dd25855e3078029bf5cc1379362c
margaretho <margaretho@users.noreply.github.com>
2015-02-11 19:26:38 -0500

    moved var message up

--- scripts/03.js
+++ scripts/03.js
@@ -1,5 +1,6 @@
 src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"
-
+        var message = document.getElementById("error");
+        
         function getLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(getWeather);
@@ -25,13 +26,11 @@ $(document).ready(function(){
 
     $("button#weather").click(function(){
         getLocation();
-        
+
         var text = $('#new-sticky').val();
         $( ".container" ).append('<div class="sticky"> <button id="close">x</button><p>' + text + '</p></div>');
         $("button#close").click(function(){
         this.parentNode.parentNode.removeChild(this.parentNode);
-
-        var message = document.getElementById("error");
     });
     });
     
